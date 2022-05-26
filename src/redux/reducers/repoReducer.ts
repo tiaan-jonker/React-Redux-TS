a09059be6b2c2b502d9c39f071e1fff23abb31cc
tiaan-jonker <tiaanj06@gmail.com>
2022-05-26 16:24:07 +1200

    reducers with type checking setup

--- src/redux/reducers/repoReducer.ts
+++ src/redux/reducers/repoReducer.ts
@@ -4,13 +4,35 @@ interface RepoState {
   data: string[]
 }
 
-const reducer = (state: RepoState, action: any) => {
+interface SearchRepoAction {
+  type: ActionType.SEARCH_REPO
+}
+
+interface SearchRepoSuccessAction {
+  type: ActionType.SEARCH_REPO_SUCCESS
+  payload: string[]
+}
+
+interface SearchRepoErrorAction {
+  type: ActionType.SEARCH_REPO_ERROR
+  payload: string
+}
+
+type Action = SearchRepoAction | SearchRepoSuccessAction | SearchRepoErrorAction
+
+enum ActionType {
+  SEARCH_REPO = 'SEARCH_REPO',
+  SEARCH_REPO_SUCCESS = 'SEARCH_REPO_SUCCESS',
+  SEARCH_REPO_ERROR = 'SEARCH_REPO_ERROR',
+}
+
+const reducer = (state: RepoState, action: Action): RepoState => {
   switch (action.type) {
-    case 'SEARCH_REPO':
+    case ActionType.SEARCH_REPO:
       return { loading: true, error: null, data: [] }
-    case 'SEARCH_REPO_SUCCESS':
+    case ActionType.SEARCH_REPO_SUCCESS:
       return { loading: false, error: null, data: action.payload }
-    case 'SEARCH_REPO_ERROR':
+    case ActionType.SEARCH_REPO_ERROR:
       return { loading: false, error: action.payload, data: [] }
     default:
       return state
